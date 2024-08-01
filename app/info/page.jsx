28437831d8346098186507add11fc5a0ddaf828a
IndahSridianti <indahsridianti9@gmail.com>
2024-08-01 14:14:53 +0700

    update dynamic page

--- app/info/page.jsx
+++ app/info/page.jsx
@@ -1,5 +1,6 @@
 import CollectionInfoComponent from "../components/info/CollectionInfoComponent";
 import Footer from "../components/layouts/Footer";
+export const dynamic = "force-dynamic";
 
 export default function InfoPage() {
   return (
